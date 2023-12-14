2dabba57160c0c3862528cf10af47ee1766fa913
Anton Angelgardt <a.n.angelgardt@gmail.com>
2023-12-15 01:11:38 +0300

    structure

---
+++ sudokuGenerator.js
@@ -0,0 +1,9 @@
+import { GRID_SIZE } from "./utilities.js";
+
+export function generateSudoku() {
+    const sudoku = createEmptygrid();
+}
+
+function createEmptygrid() {
+    return new Array(GRID_SIZE)
+}
